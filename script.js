f2e05e87411ba124263dde1bb14a0f8f76734ff2
Monica <monica@fortalezatec.com.br>
2022-02-14 20:36:23 -0300

    Commit

--- script.js
+++ script.js
@@ -109,13 +109,82 @@ c(".pizzaInfo--addButton").addEventListener("click", () => {
   closeModal();
 });
 
+c('.menu-openner').addEventListener('click', () => {
+  if (cart.length > 0) {  //se tiver itens no carrinho, abre o menu no mobile
+    c('aside').style.left = '0';
+  }
+});
+
+c('.menu-closer').addEventListener('click', () => {
+ c('aside').style.left = '100vh';
+});
+
 function updateCart() {
+
+c('.menu-openner span').innerHTML = cart.length;
+
   if (cart.length > 0) {
     c("aside").classList.add("show");
+    c(".cart").innerHTML = ""; //zera o carrinho
+
+let subtotal = 0;
+let desconto = 0;
+let total = 0;
+
     for (let i in cart) {
-      let pizzaItem = pizzaJson.find((item) => (item.id == cart[i].id));
+      let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+      subtotal += pizzaItem.price * cart[i].qt;
+
+      let cartItem = c(".models .cart--item").cloneNode(true);
+
+      let pizzaSizeName;
+
+      switch (cart[i].size) {
+        case 0:
+          pizzaSizeName = "P";
+          break;
+        case 1:
+          pizzaSizeName = "M";
+          break;
+        case 2:
+          pizzaSizeName = "G";
+          break;
+      }
+
+      let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+      cartItem.querySelector("img").src = pizzaItem.img;
+      cartItem.querySelector(".cart--item-nome").innerHTML = pizzaName;
+      cartItem.querySelector(".cart--item--qt").innerHTML = cart[i].qt;
+      cartItem
+        .querySelector(".cart--item-qtmenos")
+        .addEventListener("click", () => {
+          if (cart[i].qt > 1) {
+            cart[i].qt--;
+          } else {
+            cart.splice(i, 1);
+          } //diminui os itens do carrinho
+          updateCart();
+        });
+      cartItem
+        .querySelector(".cart--item-qtmais")
+        .addEventListener("click", () => {
+          cart[i].qt++; //aumenta os itens do carrinho
+          updateCart(); //reatualiza o carrinho
+        });
+
+      c(".cart").append(cartItem); //mostra o item no carrinho
     }
+
+desconto = subtotal * 0.1;
+total = subtotal - desconto;
+
+c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2).replace(".", ",")}`;
+c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2).replace(".", ",")}`;
+c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2).replace(".", ",")}`;
+
   } else {
     c("aside").classList.remove("show");
+    c('aside').style.left = '100vh;';
   }
 }
